b5e0b3827e95d145f3dc4928dce685ef0f57423c
irkaal <irkaalv@gmail.com>
2020-09-07 16:46:21 -0700

    Update ptriang

--- R/ptriang.R
+++ R/ptriang.R
@@ -1,48 +1,53 @@
 #' @include dtriang.R
 #' @rdname Triangular
 #' @export
-ptriang <- function(q, min = 0, max = 1, mode = (min+max)/2, lower.tail = TRUE,
-                    log.p = FALSE) {
-
-  # TODO: The numerical arguments other than n are recycled to the length of the
-  # result. Only the first elements of the logical arguments are used.
-
-  if (anyNA(c(q, min, max, mode)) || !is.numeric(c(q, min, max, mode)) ||
-      !is.logical(c(lower.tail, log.p))) {
+ptriang <- function(q, min = 0, max = 1, mode = (min + max)/2,
+                    lower.tail = TRUE, log.p = FALSE) {
+  mode <- tryCatch(mode, error = function(e) {
+    invisible(structure(list(), class = "try-error"))
+  })
+  if (class(mode) == "try-error" ||
+      anyNA(mode) || is.null(mode) || !is.numeric(mode) ||
+      anyNA(q) || anyNA(min) || anyNA(max) ||
+      is.null(q) || is.null(min) || is.null(max) ||
+      !is.numeric(q) || !is.numeric(min) || !is.numeric(max) ||
+      !is.logical(lower.tail) || !is.logical(log.p)) {
     stop("invalid arguments")
   }
-  if (length(min) > 1) {
-    min <- min[1]
-  }
-  if (length(max) > 1) {
-    max <- max[1]
-  }
-  if (length(mode) > 1) {
-    mode <- mode[1]
-  }
-  if (length(lower.tail) > 1) {
-    lower.tail <- lower.tail[1]
-  }
-  if (length(log.p) > 1) {
-    log.p <- log.p[1]
-  }
-  if (any(is.infinite(c(min, max, mode))) ||
-      mode < min || mode > max || min == max) {
+
+  len <- max(length(q), length(min), length(max), length(mode))
+  q <- rep(q, length.out = len)
+  min <- rep(min, length.out = len)
+  max <- rep(max, length.out = len)
+  mode <- rep(mode, length.out = len)
+
+  q[is.infinite(min) | is.infinite(max) | is.infinite(mode)] <- NaN
+  q[mode < min | mode > max] <- NaN
+  q[min == max] <- NaN
+
+  z <- which(q <= min)
+  l <- which(min < q & q <= mode)
+  u <- which(mode < q & q < max)
+  o <- which(max <= q)
+
+  q[z] <- 0
+  q[l] <- (q[l] - min[l])^2 / (max[l] - min[l]) / (mode[l] - min[l])
+  q[u] <- 1 - (max[u] - q[u])^2 / (max[u] - min[u]) / (max[u] - mode[u])
+  q[o] <- 1
+
+  # If lower.tail or log.p has length > 1, only the first element will be used
+  suppressWarnings({
+    if (!lower.tail) {
+      q <- 1 - q
+    }
+    if (log.p) {
+      q <- log(q)
+    }
+  })
+
+
+  if (anyNA(q)) {
     warning("NaNs produced")
-    return(rep(NaN, length(q)))
   }
-
-  zero <- q <= min
-  lower <- min < q & q <= mode
-  upper <- mode < q & q < max
-  one <- max <= q
-  q[zero] <- 0
-  q[lower] <- (q[lower] - min)^2 / (max - min) / (mode - min)
-  q[upper] <- 1 - (max - q[upper])^2 / (max - min) / (max - mode)
-  q[one] <- 1
-
-  if (!lower.tail) {
-    q <- 1 - q
-  }
-  if (log.p) log(q) else q
+  q
 }
