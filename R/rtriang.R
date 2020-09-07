57d801432034422e9fcf2f716283ea799145a9c4
irkaal <irkaalv@gmail.com>
2020-09-07 01:13:20 -0700

    Vectorize qtriang and rtriang

--- R/rtriang.R
+++ R/rtriang.R
@@ -2,33 +2,38 @@
 #' @rdname Triangular
 #' @importFrom stats runif
 #' @export
-rtriang <- function(n, min = 0, max = 1, mode = (min+max)/2) {
-  p <- tryCatch(stats::runif(n), error = function(c) NaN)
-  if (length(p) == 0 || (length(p) == 1 && is.nan(p)) ||
-      anyNA(c(min, max, mode)) || !is.numeric(c(min, max, mode))) {
+rtriang <- function(n, min = 0, max = 1, mode = (min + max)/2) {
+  mode <- try(mode, silent = TRUE)
+  if (class(mode) == "try-error") {
     stop("invalid arguments")
   }
 
-  if (length(min) > 1) {
-    min <- min[1]
-  }
-  if (length(max) > 1) {
-    max <- max[1]
-  }
-  if (length(mode) > 1) {
-    mode <- mode[1]
-  }
-  if (any(is.infinite(c(min, max, mode))) || mode < min || mode > max) {
-    warning("NaNs produced")
-    return(rep(NaN, length(p)))
-  }
-  if (min == max) {
-    return(rep(min, length(p)))
+  p <- try(stats::runif(n, min = 0, max = 1), silent = TRUE)
+  if (class(p) == "try-error") {
+    stop("invalid arguments")
   }
 
-  lower <- p < (mode - min) / (max - min)
-  c(
-    min + sqrt(p[lower] * (max - min) * (mode - min)),
-    max - sqrt((1 - p[!lower]) * (max - min) * (max - mode))
-  )
+  if (anyNA(c(min, max, mode)) ||
+      any(is.null(min), is.null(max), is.null(mode)) ||
+      !is.numeric(c(min, max, mode))) {
+    stop("invalid arguments")
+  }
+
+  if (length(c(min, max, mode)) == 3) {
+    if (any(is.infinite(c(min, max, mode)), mode < min, mode > max)) {
+      warning("NaNs produced")
+      return(rep(NaN, length(p)))
+    }
+
+    if (min == max) {
+      return(rep(min, length(p)))
+    }
+
+    l <- p < (mode - min) / (max - min)
+    p[l] <- min + sqrt(p[l] * (max - min) * (mode - min))
+    p[!l] <- max - sqrt((1 - p[!l]) * (max - min) * (max - mode))
+    p
+  } else {
+    qtriang(p, min, max, mode, lower.tail = TRUE, log.p = FALSE)
+  }
 }
